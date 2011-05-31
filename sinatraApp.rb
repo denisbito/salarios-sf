dae5b53e703e8a494c8f78bbc14a937ae38fa4de
Denis Oliveira <denisbito@gmail.com>
2011-05-31 20:15:30 -0300

    - biblioteca gráfica trocada: Highcharts no lugar da Protovis.

--- sinatraApp.rb
+++ sinatraApp.rb
@@ -1,7 +1,12 @@
 # sintatraApp.rb
-require 'rubygems'
 require 'sinatra'
 
+# redirecionar para a página inicial
 get '/' do
-	'Hello World, from Sinatra!'
+	File.read(File.join('public', 'index-highcharts.html'))
+end
+
+# obter os dados do gráfico
+get '/dados' do
+	return '<raiz><exemplo><nome>Denis Oliveira</nome></exemplo></raiz>'
 end
